1c68c1dbeb9e3ac7d8fcd3d6aca981277e696254
kimbyeongnyeon <qud5252@naver.com>
2025-12-18 01:29:31 +0900

    feat(#18): 메인에 스크롤 스타일 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { Providers } from "./lib/providers.tsx";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/scrollbar.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
