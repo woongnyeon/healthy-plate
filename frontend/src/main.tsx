bc6afdd57c8d591d963664ca717c3c96521cb5e6
kimbyeongnyeon <qud5252@naver.com>
2025-12-17 22:40:28 +0900

    feat(#18): 브라우저 라우터 추가

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Providers } from "./lib/providers.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Providers>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Providers>
   </StrictMode>
 );
