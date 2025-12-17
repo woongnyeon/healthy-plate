80da7be0e8f0bfa71b2bc5f035bfb95fd3a0b531
kimbyeongnyeon <qud5252@naver.com>
2025-12-17 22:39:57 +0900

    feat(#18): 라우팅 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { TrendPage } from "./recipe/pages/TrandPage";
+import { RecipeLayout } from "./recipe/RecipeLayout";
+
 function App() {
   return (
-    <div className="bg-red-500">
-      <h3 className="text-black">Hi</h3>
+    <div className="flex min-h-screen bg-bg">
+      <main className="flex-1">
+        <Routes>
+          <Route element={<RecipeLayout />}>
+            <Route path="/" element={<TrendPage />} />
+            <Route path="/chef" element={<TrendPage />} />
+          </Route>
+        </Routes>
+      </main>
     </div>
   );
 }
