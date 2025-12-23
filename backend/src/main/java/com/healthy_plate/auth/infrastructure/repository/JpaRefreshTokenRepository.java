eff9356993dba4b303f370aeafe4a865d0bb4d13
dbsdndcks <ywcsuwon@naver.com>
2025-12-23 16:51:09 +0900

    Revert "Merge pull request #36 from woongnyeon/be/google-oauth-login"
    This reverts commit be280a5b3457da86f0d2a25326b8c3b3834e041d, reversing
    changes made to 62b5ee715c11cd3f0c96c5741b1a5fa5b60814cd.

--- backend/src/main/java/com/healthy_plate/auth/infrastructure/repository/JpaRefreshTokenRepository.java
+++
@@ -1,9 +0,0 @@
-package com.healthy_plate.auth.infrastructure.repository;
-
-import com.healthy_plate.auth.domain.model.RefreshToken;
-import com.healthy_plate.auth.domain.repository.RefreshTokenRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaRefreshTokenRepository extends JpaRepository<RefreshToken, Long>, RefreshTokenRepository {
-
-}
--- backend/src/main/java/com/healthy_plate/user/domain/model/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.healthy_plate.user.domain.model;
-
-public enum UserRole {
-    ROLE_USER, ROLE_ADMIN
-}
