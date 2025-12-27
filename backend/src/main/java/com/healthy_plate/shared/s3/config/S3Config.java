82ccaf72d2c9bf5a7d9592df044676228dc9d060
dbsdndcks <ywcsuwon@naver.com>
2025-12-27 12:54:56 +0900

    refactor(#33) : 패키지 이동

--- backend/src/main/java/com/healthy_plate/shared/s3/config/S3Config.java
+++ backend/src/main/java/com/healthy_plate/shared/s3/config/S3Config.java
@@ -1,4 +1,4 @@
-package com.healthy_plate.shared.s3;
+package com.healthy_plate.shared.s3.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
